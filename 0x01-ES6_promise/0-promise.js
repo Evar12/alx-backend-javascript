267078487f57f2513cd9d0d91fda130170246dfd
Evar12 <Evaristus12@gmail.com>
2024-06-27 21:32:18 +0100

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,4 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Simulate an API call with setTimeout
-      setTimeout(() => {
-        const success = true; // Simulate whether the API call was successful
-        if (success) {
-          resolve("Success!"); // Resolve the promise with a success message
-        } else {
-          reject("Error!"); // Reject the promise with an error message
-        }
-      }, 1000); // Simulate a delay of 1 second
-    });
-  }
-  
-  export default getResponseFromAPI;
+/* eslint-disable */
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
+}
